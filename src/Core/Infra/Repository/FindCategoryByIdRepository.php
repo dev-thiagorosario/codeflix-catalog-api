0e5474cc6d03d9f8811d9fac4b08e6213e2740b8
dev-thiagorosario <eng.thiagosouza@icloud.com>
2026-05-14 17:35:55 -0300

    feat(category): add FindCategoryByIdRepository implementation
    - Introduced `FindCategoryByIdRepository` to retrieve `CategoryEntity` by ID.
    - Mapped `Category` model data to `CategoryEntity` using `fromModel` method.

---
+++ src/Core/Infra/Repository/FindCategoryByIdRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Infra\Repository;
+
+use App\Core\Domain\Entity\CategoryEntity;
+use App\Core\Domain\Repository\CategoryRepositoryInterface;
+use App\Models\Category;
+
+abstract class FindCategoryByIdRepository implements CategoryRepositoryInterface
+{
+    public function findById(string $id): ?CategoryEntity
+    {
+        $model = Category::query()->find($id);
+
+        if ($model === null) {
+            return null;
+        }
+
+        return CategoryEntity::fromModel($model);
+    }
+}

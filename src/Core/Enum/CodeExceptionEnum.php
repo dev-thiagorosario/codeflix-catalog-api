f024571e59b7e815b9c5bc3a567cda8c68d443ea
dev-thiagorosario <eng.thiagosouza@icloud.com>
2026-05-18 19:23:37 -0300

    feat(enum): add new exception codes for category and JSend errors
    - Added `CREATE_CATEGORY_ERROR`, `INVALID_JSEND_STATUS`, and `JSEND_ERROR_MESSAGE_REQUIRED` to `CodeExceptionEnum`.

--- src/Core/Enum/CodeExceptionEnum.php
+++ src/Core/Enum/CodeExceptionEnum.php
@@ -8,4 +8,7 @@ enum CodeExceptionEnum: int
 {
     case ENTITY_VALIDATION = 1003;
     case CATEGORY_NOT_FOUND = 1004;
+    case CREATE_CATEGORY_ERROR = 1005;
+    case INVALID_JSEND_STATUS = 1006;
+    case JSEND_ERROR_MESSAGE_REQUIRED = 1007;
 }
